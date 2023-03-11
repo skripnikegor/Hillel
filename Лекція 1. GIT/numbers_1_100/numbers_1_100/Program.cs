282a7ce68c3cb489d39d1f63a510f1ab86858e0f
Egor <34675756+skripnikegor@users.noreply.github.com>
2023-03-11 09:52:14 +0200

    Added folders and programm

---
+++ Лекція 1. GIT/numbers_1_100/numbers_1_100/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numbers_1_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
