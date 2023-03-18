de4d64150ecf43d84a43eece12b985f892fbef52
Egor <34675756+skripnikegor@users.noreply.github.com>
2023-03-18 13:08:26 +0200

    added lesson 4

---
+++ Занятие 4. Статичні і вкладені класи. Узагальнення/Lesson4/Lesson4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Lesson4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var str1 = "1111";
+            var str2 = "2222";
+
+            var strBuilder = new StringBuilder();
+            strBuilder.Append(str1);
+            strBuilder.Append(str2);
+
+            var res = strBuilder.ToString();
+            Console.WriteLine(res);
+
+            str1 = "3333";
+
+            Console.WriteLine(res);
+            Console.WriteLine(strBuilder);
+
+            Console.ReadKey();
+        }
+    }
+}
