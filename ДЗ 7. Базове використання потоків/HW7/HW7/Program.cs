40ff87f5acf6291dae4894ac919939a831ae2a63
Egor <34675756+skripnikegor@users.noreply.github.com>
2023-04-02 11:33:39 +0300

    Added HW7

---
+++ ДЗ 7. Базове використання потоків/HW7/HW7/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HW7
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<int> numbers = Enumerable.Range(1, 100000).ToList();
+            List<int> results = new List<int>();
+
+
+            Func<int, int, List<int>> FindSquares = (from, to) =>
+            {
+                List<int> ints = new List<int>();
+                for (int i = from; i < to; i++)
+                {
+                    if (Math.Sqrt(numbers[i]) % 1 == 0)
+                    {
+                        ints.Add(numbers[i]);
+                    }
+                }
+                return ints;
+            };
+
+
+            int threadsQuantity = 4;
+            Thread[] threads = new Thread[threadsQuantity];
+            int Volume = numbers.Count/threadsQuantity;
+
+
+            for (int i = 0; i < threadsQuantity; i++)
+            {
+                int start = i * Volume;
+                int finish = start + Volume;
+
+                threads[i] = new Thread(() =>
+                {
+                    List<int> threadResult = FindSquares(start, finish);
+                    lock (results)
+                    {
+                        results.AddRange(threadResult);
+                    }
+                });
+                threads[i].Start();
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+
+            foreach(var result in results)
+            {
+                Console.WriteLine(result);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Занятие 6-7. Потоки/Less6-7/Less6-7/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Less6_7
+{
+    internal class Program
+    {
+        private static int count = 0;
+        static void Main(string[] args)
+        {
+            var lockObject = new Object();
+            //Thread t1 = new Thread(() =>
+            //{
+            //    for (int i = 0; i < 1000000; i++)
+            //    {
+            //        lock (lockObject)
+            //        {
+            //            count++;
+            //        }
+
+            //    }
+            //});
+
+
+            //Thread t2 = new Thread(() =>
+            //{
+            //    for (int i = 0; i < 1000000; i++)
+            //    {
+            //        lock (lockObject)
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //});
+
+            //t1.Start();
+            //t2.Start();
+
+            //t1.Join();
+            //t2.Join();
+
+
+
+
+            Thread[] threads = new Thread[10];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(() =>
+                {
+                    lock (lockObject)
+                    {
+                        count += 10;
+                    }
+                });
+            }
+
+            foreach (var t in threads)
+                {
+                    t.Start();
+                }
+
+            foreach (var t in threads)
+            {
+                t.Join();
+            }
+
+
+            Console.WriteLine($"{count}");
+
+
+
+            Console.ReadLine();
+            
+        }
+    }
+}
