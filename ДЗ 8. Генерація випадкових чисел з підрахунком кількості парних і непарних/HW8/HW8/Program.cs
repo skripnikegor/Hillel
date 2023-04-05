81bb01e125365a535a79fbc07dd378e2f8a28040
Egor <34675756+skripnikegor@users.noreply.github.com>
2023-04-05 18:13:12 +0300

    Added hw8

---
+++ ДЗ 8. Генерація випадкових чисел з підрахунком кількості парних і непарних/HW8/HW8/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HW8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object locker = new object();
+            Random random = new Random();
+            int even = 0;
+            int odd = 0;
+
+            List<Thread> threads = new List<Thread>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                threads.Add(new Thread(Counter));
+                threads[i].Name = $"Thread {i}";
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Start();
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+
+            Console.WriteLine($"Even numbers: {even}, odd numbers: {odd}");
+
+            Console.ReadKey();
+
+            void Counter()
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    int rand = random.Next(0, 100);
+                    //для наглядности
+                    Console.WriteLine($"{Thread.CurrentThread.Name}, number {rand} ");
+                    lock (locker)
+                    {
+                        if (rand % 2 == 0)
+                        {
+                            even += 1;
+                        }
+                        else
+                        {
+                            odd += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
