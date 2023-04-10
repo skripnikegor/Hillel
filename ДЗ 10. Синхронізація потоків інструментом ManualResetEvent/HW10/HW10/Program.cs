a0d1c3ef7018d21e8ecfb8f4ad5d8c68a4017f5f
Egor <34675756+skripnikegor@users.noreply.github.com>
2023-04-10 19:56:27 +0300

    Added HW10

---
+++ ДЗ 10. Синхронізація потоків інструментом ManualResetEvent/HW10/HW10/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HW10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            List<int> list = new List<int>();
+            int result = 0;
+
+            //Add elements to list - ints from 1 to 20 (for checking)
+            for (int i = 0; i < 100000; i++)
+            {
+                list.Add(rnd.Next(1,20));
+            }
+
+            //Create mre objects for different functions
+            ManualResetEvent mreChecker = new ManualResetEvent(false);
+            ManualResetEvent mreSummer = new ManualResetEvent(false);
+
+            //Create Thread for checking and run it
+            Console.WriteLine("Run checker");
+            Thread check = new Thread(() => Checker(list, mreChecker));
+            check.Start();
+            mreChecker.WaitOne();
+
+            //Create Thread for summaring and run it
+            Console.WriteLine("Run summer");
+            Thread summer = new Thread(() => result += Summer(list, mreSummer));
+            summer.Start();
+            mreSummer.WaitOne();
+
+            Console.WriteLine($"Result is {result}");
+
+            Console.ReadLine();
+        }
+
+
+        public static int Summer(List<int> list, ManualResetEvent mre)
+        {
+            int result = 0;
+            int threadsQty = 4;
+            int quantity = list.Count / threadsQty;
+
+            for (int i = 0; i < threadsQty; i++)
+            {
+                Thread th = new Thread(() => result += SummerWorker(list.Skip(i * quantity).Take(quantity).ToList()));
+                th.Name = i.ToString();
+                th.Start();
+                th.Join();
+            }                
+
+            Console.WriteLine("Summer finished work");
+            mre.Set();
+
+            return result;
+        }
+
+        public static int SummerWorker(List<int> list)
+        {
+            int result = 0;
+            foreach(int i in list)
+            {
+                result += i;
+            }
+            Console.WriteLine($"Result from summerWorker {Thread.CurrentThread.Name}: {result}");
+            return result;
+        }
+
+        public static void Checker(List<int> list, ManualResetEvent mre)
+        {
+            foreach (int i in list)
+            {
+                if (list[i] <= 0)
+                {
+                    Console.WriteLine("I have found a number less than zero!"); 
+                }
+                    
+            }
+            Console.WriteLine("Checker finished work");
+            mre.Set();
+        }
+
+    }
+}
---
+++ Занятие 10. Task/Lesson10/Lesson10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lesson10
+{
+    internal class Program
+    {
+        static async void Main(string[] args)
+        {
+            MakePreparations();
+        }
+        static async Task<int> GetSomeValue()
+        {
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(200);
+                return 97;
+            });
+        }
+
+        static async void MakePreparations()
+        {
+            var result = await GetSomeValue();
+
+            Console.WriteLine("Some Preparations");
+            Thread.Sleep(500);
+            Console.WriteLine("Finished Preparations");
+
+            Console.Write(result + 1);
+
+        }
+
+    }
+}
