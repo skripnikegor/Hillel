1ebb0da6adec0d456fbce899b7d91fecbdf67ca4
Egor <34675756+skripnikegor@users.noreply.github.com>
2023-05-27 19:02:12 +0300

    Added hw17 and 19

---
+++ ДЗ 19. Додаток для HTTP запитів/HW19_c/Model/Sender.cs
@@ -0,0 +1,102 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW19_c.Model
+{
+    public static class Sender
+    {
+        public async static Task Get()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("https://postman-echo.com/get");
+                    response.EnsureSuccessStatusCode();
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Get");
+                    Console.WriteLine(responseBody);
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        public async static Task Post()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var requestBody = new { id = 1, title = "hello" };
+                    var jsonContent = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await client.PostAsync("https://postman-echo.com/post", jsonContent);
+                    response.EnsureSuccessStatusCode();
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Post");
+                    Console.WriteLine(responseBody);
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        public async static Task Put()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var requestBody = new { id = 1, title = "hello" };
+                    var jsonContent = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await client.PutAsync("https://postman-echo.com/put", jsonContent);
+                    response.EnsureSuccessStatusCode();
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Put");
+                    Console.WriteLine(responseBody);
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        public async static Task Delete()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.DeleteAsync("https://postman-echo.com/delete");
+                    response.EnsureSuccessStatusCode();
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Delete");
+                    Console.WriteLine(responseBody);
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+
+
+
+
+    }
+}
---
+++ ДЗ 19. Додаток для HTTP запитів/HW19_c/Program.cs
@@ -0,0 +1,29 @@
+﻿using HW19_c.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW19_c
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Sender.Get();
+            
+            Sender.Post();
+
+            Sender.Put();
+
+            Sender.Delete();
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
