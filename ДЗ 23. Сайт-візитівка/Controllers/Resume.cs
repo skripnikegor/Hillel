e8a52239ee74434a7423ff53d3392e7a559ae8e6
Egor <34675756+skripnikegor@users.noreply.github.com>
2023-06-24 11:40:04 +0300

    Added hw 23

---
+++ ДЗ 23. Сайт-візитівка/Controllers/Resume.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ДЗ_23._Сайт_візитівка.Controllers
+{
+    public class Resume : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Education()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View("About");
+        }
+    }
+}
